e5cbae76272008b061d3e1a126d123b163cbd3a1
iguissouma <issam.guissouma@laposte.net>
2020-10-03 18:50:31 +0200

    feature(project): add icon for app and lib folders

---
+++ src/main/kotlin/com/github/iguissouma/nxconsole/NxDirectoryIconProvider.kt
@@ -0,0 +1,46 @@
+package com.github.iguissouma.nxconsole
+
+import com.github.iguissouma.nxconsole.buildTools.NxJsonUtil
+import com.intellij.icons.AllIcons
+import com.intellij.ide.IconProvider
+import com.intellij.json.psi.JsonObject
+import com.intellij.json.psi.JsonStringLiteral
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import java.io.File
+import javax.swing.Icon
+
+class NxDirectoryIconProvider : IconProvider(), DumbAware {
+
+    override fun getIcon(element: PsiElement, flags: Int): Icon? {
+        if (element is PsiDirectory) {
+            val project: Project = element.getProject()
+            val angularJsonFile = NxJsonUtil.findChildAngularJsonFile(project.baseDir) ?: return null
+            val projectsProperty = NxJsonUtil.findProjectsProperty(project, angularJsonFile) ?: return null
+            val file: VirtualFile = element.virtualFile
+            val toMap = (projectsProperty.value as JsonObject)
+                .propertyList
+                .map { Triple(it.name, (it.value as? JsonObject)?.findProperty("root"), (it.value as? JsonObject)?.findProperty("projectType")) }
+                .filter { it.second != null }
+                .map {
+                    LocalFileSystem.getInstance()
+                        .findFileByIoFile(File(angularJsonFile.parent.path + "/" + (it.second?.value as? JsonStringLiteral)?.value)) to (it.first to (it.third?.value as? JsonStringLiteral)?.value)
+                }.toMap()
+
+            if (toMap.containsKey(file)) {
+                val get: Pair<String, String?>? = toMap[file]
+                if (get?.second == "application") {
+                    return NxIcons.NX_APP_FOLDER
+                } else if (get?.second == "library") {
+                    return NxIcons.NX_LIB_FOLDER
+                }
+            }
+
+        }
+        return null
+    }
+}
--- src/main/kotlin/com/github/iguissouma/nxconsole/NxIcons.kt
+++ src/main/kotlin/com/github/iguissouma/nxconsole/NxIcons.kt
@@ -4,6 +4,8 @@ import com.intellij.openapi.util.IconLoader
 
 object NxIcons {
     val NRWL_ICON = IconLoader.getIcon("/icons/nrwl.svg")
+    val NX_APP_FOLDER = IconLoader.getIcon("/icons/appFolder.svg")
+    val NX_LIB_FOLDER = IconLoader.getIcon("/icons/libForlder.svg")
     val NX_CONSOLE_ICON = IconLoader.getIcon("/icons/nx_console.svg")
     val TREE_VIEW_ICON = IconLoader.getIcon("/icons/tree_view.svg")
 }
